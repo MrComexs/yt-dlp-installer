def2fea26d00e8dfab6e7c309512377b497dc3b3
Jesus <57308435+MrComexs@users.noreply.github.com>
2022-03-02 22:22:27 -0800

    installs to C:\Program FIles\yt-dlp
    also got rid of some of the script that wasn't used.

--- yt-dlp-N-installer.bat
+++ yt-dlp-N-installer.bat
@@ -1,8 +1,5 @@
 @echo off
-::From mpv-install.bat I thought I would need it
-::setlocal enableextensions enabledelayedexpansion
-::path %SystemRoot%\System32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SystemRoot%\System32\WindowsPowerShell\v1.0\
-::call :ensure_admin
+call :ensure_admin
 
 cls
 :ask
@@ -18,61 +15,57 @@ If /I "%iu%"=="uninstall" goto uninstall
 :install
 	cd %UserProfile%\Videos\
 	mkdir %UserProfile%\Videos\yt-dlp
-	cd %UserProfile%\Videos\yt-dlp\
-	mkdir %UserProfile%\Videos\yt-dlp\yt-dlp-program-files
+	cd "C:\Program Files"
+	mkdir "C:\Program FIles\yt-dlp"
 	cd /D %~dp0\
-	move yt-dlp.exe %UserProfile%\Videos\yt-dlp\yt-dlp-program-files
-	move yt-dlp.bat %UserProfile%\Videos\yt-dlp\yt-dlp-program-files
-	move yt-dlp.conf %UserProfile%\Videos\yt-dlp\yt-dlp-program-files
-	move ffmpeg.exe  %UserProfile%\Videos\yt-dlp\yt-dlp-program-files
-	move ffprobe.exe  %UserProfile%\Videos\yt-dlp\yt-dlp-program-files
+	move yt-dlp.exe "C:\Program Files\yt-dlp"
+	move yt-dlp.bat "C:\Program Files\yt-dlp"
+	move yt-dlp.conf "C:\Program Files\yt-dlp"
+	move ffmpeg.exe  "C:\Program Files\yt-dlp"
 	cd /D %~dp0
 	move yt-dlp.lnk "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
 	cls
-	Echo yt-dlp.exe was intsalled in "%UserProfile%\Videos\yt-dlp\yt-dlp-program-files\"
-	Echo yt-dlp.bat was intsalled in "%UserProfile%\Videos\yt-dlp\yt-dlp-program-files\"
-	echo ffmpeg.exe was installed in ffmpeg.exe  "%UserProfile%\Videos\yt-dlp\yt-dlp-program-files\"
-	echo ffprobe.exe was installed in ffprobe.exe  "%UserProfile%\Videos\yt-dlp\yt-dlp-program-files\"
-	Echo yt-dlp.lnk was intsalled in "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
+	Echo yt-dlp.exe was intsalled in "C:\Program FIles\yt-dlp"
+	Echo yt-dlp.bat was intsalled in "C:\Program FIles\yt-dlp"
+	echo ffmpeg.exe was installed in ffmpeg.exe  "C:\Program FIles\yt-dlp"
+	Echo yt-dlp.lnk was intsalled in "C:\Program FIles\yt-dlp"
 	Echo.
 	echo Run yt-dlp(.lnk) to download video.
 	echo.
 	:ask
 	echo Would you like to see the supported site to download from?:
-	set /P INPUT=yes or no: %=%
-	If "%INPUT%"=="y" goto yes 
-	If "%INPUT%"=="n" goto no
-	If "%INPUT%"=="Y" goto yes
-	If "%INPUT%"=="N" goto no
+	set /P INPUT=(Yes\No): %=%
+	If /I "%INPUT%"=="y" goto yes 
+	If /I "%INPUT%"=="n" goto no
+	If /I "%INPUT%"=="yes" goto yes
+	If /I "%INPUT%"=="no" goto no
 	echo.
 	pause
 	exit
 
 :uninstall
-	del %UserProfile%\Videos\yt-dlp\yt-dlp-program-files\yt-dlp.bat
-	del %UserProfile%\Videos\yt-dlp\yt-dlp-program-files\yt-dlp.exe
-	del %UserProfile%\Videos\yt-dlp\yt-dlp-program-files\yt-dlp.conf
-	del %UserProfile%\Videos\yt-dlp\yt-dlp-program-files\ffmpeg.exe
-	del %UserProfile%\Videos\yt-dlp\yt-dlp-program-files\ffprobe.exe
-	rmdir %UserProfile%\Videos\yt-dlp\yt-dlp-program-files\
+	del "C:\Program Files\yt-dlp\yt-dlp.bat"
+	del "C:\Program Files\yt-dlp\yt-dlp.exe"
+	del "C:\Program Files\yt-dlp\yt-dlp.conf"
+	del "C:\Program Files\yt-dlp\ffmpeg.exe"
+	rmdir "C:\Program Files\yt-dlp"
 	del "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\yt-dlp.lnk"
-	Echo yt-dlp.exe was unintsalled from "%UserProfile%%UserProfile%\Videos\yt-dlp\"
-	Echo yt-dlp.bat was unintsalled from "%UserProfile%%UserProfile%\Videos\yt-dlp\"
-	Echo yt-dlp.conf was unintsalled from "%UserProfile%%UserProfile%\Videos\yt-dlp\"
-	Echo ffmpeg.exe was unintsalled from "%UserProfile%%UserProfile%\Videos\yt-dlp\"
-	Echo ffprobe.exe was unintsalled from "%UserProfile%%UserProfile%\Videos\yt-dlp\"
-	Echo yt-dlp.lnk was unintsalled from "%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\yt-dlp.lnk"
+	Echo yt-dlp.exe was unintsalled from "C:\Program Files\yt-dlp"
+	Echo yt-dlp.bat was unintsalled from "C:\Program Files\yt-dlp"
+	Echo yt-dlp.conf was unintsalled from "C:\Program Files\yt-dlp"
+	Echo ffmpeg.exe was unintsalled from "C:\Program Files\yt-dlp"
 	echo.
 	pause
 	exit
 
-::ensure_admin
 	:: From mpv-install.bat
+:ensure_admin
 	:: https://stackoverflow.com/questions/4051883/batch-script-how-to-check-for-admin-rights
 	openfiles >nul 2>&1
 	if errorlevel 1 (
 		echo This batch script requires administrator privileges.
 		echo Right-click on yt-dlp-N-installer.bat and select "Run as administrator".
+		echo Thank you mpv team for this part of the script
 		echo.
 		call :die
 	)
